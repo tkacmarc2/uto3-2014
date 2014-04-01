da8186f06ca63ed07ca6af28537bffef658a8dde
Tomislav Kačmarčik <tkacmarc2@foi.hr>
2014-04-01 14:08:05 +0200

    završena sučelja

--- uto3-2014/Program.cs
+++ uto3-2014/Program.cs
@@ -16,7 +16,7 @@ namespace uto3_2014
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmGlavna());
         }
     }
 }
---
+++ uto3-2014/frmGlavna.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace uto3_2014
+{
+    partial class frmGlavna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPolumjer = new System.Windows.Forms.Label();
+            this.lblPovrsina = new System.Windows.Forms.Label();
+            this.lblOpseg = new System.Windows.Forms.Label();
+            this.txtPolumjer = new System.Windows.Forms.TextBox();
+            this.txtPovrsina = new System.Windows.Forms.TextBox();
+            this.txtOpseg = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chkSpremi = new System.Windows.Forms.CheckBox();
+            this.btnIzracunaj = new System.Windows.Forms.Button();
+            this.btnOcisti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPolumjer
+            // 
+            this.lblPolumjer.AutoSize = true;
+            this.lblPolumjer.Location = new System.Drawing.Point(12, 9);
+            this.lblPolumjer.Name = "lblPolumjer";
+            this.lblPolumjer.Size = new System.Drawing.Size(50, 13);
+            this.lblPolumjer.TabIndex = 0;
+            this.lblPolumjer.Text = "Polumjer:";
+            // 
+            // lblPovrsina
+            // 
+            this.lblPovrsina.AutoSize = true;
+            this.lblPovrsina.Location = new System.Drawing.Point(12, 33);
+            this.lblPovrsina.Name = "lblPovrsina";
+            this.lblPovrsina.Size = new System.Drawing.Size(51, 13);
+            this.lblPovrsina.TabIndex = 1;
+            this.lblPovrsina.Text = "Površina:";
+            // 
+            // lblOpseg
+            // 
+            this.lblOpseg.AutoSize = true;
+            this.lblOpseg.Location = new System.Drawing.Point(12, 59);
+            this.lblOpseg.Name = "lblOpseg";
+            this.lblOpseg.Size = new System.Drawing.Size(41, 13);
+            this.lblOpseg.TabIndex = 2;
+            this.lblOpseg.Text = "Opseg:";
+            // 
+            // txtPolumjer
+            // 
+            this.txtPolumjer.Location = new System.Drawing.Point(68, 6);
+            this.txtPolumjer.Name = "txtPolumjer";
+            this.txtPolumjer.Size = new System.Drawing.Size(100, 20);
+            this.txtPolumjer.TabIndex = 3;
+            this.txtPolumjer.Text = "0,00";
+            this.txtPolumjer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPovrsina
+            // 
+            this.txtPovrsina.Location = new System.Drawing.Point(68, 33);
+            this.txtPovrsina.Name = "txtPovrsina";
+            this.txtPovrsina.ReadOnly = true;
+            this.txtPovrsina.Size = new System.Drawing.Size(100, 20);
+            this.txtPovrsina.TabIndex = 4;
+            this.txtPovrsina.Text = "0,00";
+            this.txtPovrsina.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtOpseg
+            // 
+            this.txtOpseg.Location = new System.Drawing.Point(68, 59);
+            this.txtOpseg.Name = "txtOpseg";
+            this.txtOpseg.ReadOnly = true;
+            this.txtOpseg.Size = new System.Drawing.Size(100, 20);
+            this.txtOpseg.TabIndex = 5;
+            this.txtOpseg.Text = "0,00";
+            this.txtOpseg.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // chkSpremi
+            // 
+            this.chkSpremi.AutoSize = true;
+            this.chkSpremi.Location = new System.Drawing.Point(68, 85);
+            this.chkSpremi.Name = "chkSpremi";
+            this.chkSpremi.Size = new System.Drawing.Size(88, 17);
+            this.chkSpremi.TabIndex = 7;
+            this.chkSpremi.Text = "Spremi u listu";
+            this.chkSpremi.UseVisualStyleBackColor = true;
+            // 
+            // btnIzracunaj
+            // 
+            this.btnIzracunaj.Location = new System.Drawing.Point(68, 108);
+            this.btnIzracunaj.Name = "btnIzracunaj";
+            this.btnIzracunaj.Size = new System.Drawing.Size(75, 23);
+            this.btnIzracunaj.TabIndex = 8;
+            this.btnIzracunaj.Text = "Izračunaj";
+            this.btnIzracunaj.UseVisualStyleBackColor = true;
+            // 
+            // btnOcisti
+            // 
+            this.btnOcisti.Location = new System.Drawing.Point(68, 137);
+            this.btnOcisti.Name = "btnOcisti";
+            this.btnOcisti.Size = new System.Drawing.Size(75, 23);
+            this.btnOcisti.TabIndex = 9;
+            this.btnOcisti.Text = "Očisti";
+            this.btnOcisti.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 195);
+            this.Controls.Add(this.btnOcisti);
+            this.Controls.Add(this.btnIzracunaj);
+            this.Controls.Add(this.chkSpremi);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtOpseg);
+            this.Controls.Add(this.txtPovrsina);
+            this.Controls.Add(this.txtPolumjer);
+            this.Controls.Add(this.lblOpseg);
+            this.Controls.Add(this.lblPovrsina);
+            this.Controls.Add(this.lblPolumjer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPolumjer;
+        private System.Windows.Forms.Label lblPovrsina;
+        private System.Windows.Forms.Label lblOpseg;
+        private System.Windows.Forms.TextBox txtPolumjer;
+        private System.Windows.Forms.TextBox txtPovrsina;
+        private System.Windows.Forms.TextBox txtOpseg;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox chkSpremi;
+        private System.Windows.Forms.Button btnIzracunaj;
+        private System.Windows.Forms.Button btnOcisti;
+    }
+}
+
